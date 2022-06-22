126852febbf365854667785439a207c9e35b15fd
Harshad <harsh.haou@gmail.com>
2022-06-22 13:47:16 +0530

    build

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,18 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyChhKZ6g3bx6wcRiirlb3-XStF1AIigZdk',
-  authDomain: 'sales-app-f893f.firebaseapp.com',
-  projectId: 'sales-app-f893f',
-  storageBucket: 'sales-app-f893f.appspot.com',
-  messagingSenderId: '410612547449',
-  appId: '1:410612547449:web:552d3d3ed244a8babbf9a8',
+  // apiKey: 'AIzaSyChhKZ6g3bx6wcRiirlb3-XStF1AIigZdk',
+  // authDomain: 'sales-app-f893f.firebaseapp.com',
+  // projectId: 'sales-app-f893f',
+  // storageBucket: 'sales-app-f893f.appspot.com',
+  // messagingSenderId: '410612547449',
+  // appId: '1:410612547449:web:552d3d3ed244a8babbf9a8',
+  apiKey: 'AIzaSyDXjB6JznigvqmHjBvNO0lc_sMHRzwA9a4',
+  authDomain: 'sales-app-b93f0.firebaseapp.com',
+  projectId: 'sales-app-b93f0',
+  storageBucket: 'sales-app-b93f0.appspot.com',
+  messagingSenderId: '40375088435',
+  appId: '1:40375088435:web:08a74eb6ca29647f137a46',
 };
 
 const app = initializeApp(firebaseConfig);
